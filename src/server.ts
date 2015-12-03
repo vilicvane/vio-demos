707c77c7689b9ceab91fbb52833a4e1c510d7206
vilicvane <i@vilic.info>
2015-12-03 22:48:01 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,21 @@ import { Router } from 'vio';
 
 let app = express();
 
+// set default view engine.
 app.engine('hbs', handlebars);
 
+// add static file handler and other middleware if needed.
 app.use('/static', express.static(Path.join(__dirname, '../static')));
 
+// attach the "endware".
 let router = new Router(app, {
     routesRoot: Path.join(__dirname, 'routes'),
     viewsRoot: Path.join(__dirname, '../views'),
-    viewsExtension: 'hbs'
+    viewsExtension: '.hbs'
 });
 
-app.listen(1337);
+let port = process.env.PORT || 1337;
+
+app.listen(port);
+
+console.log(`listening on port ${port}...`);
